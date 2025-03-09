f941d9647462589abcf56a738d5d91106a6f5b76
Ruin0verall <96787822+Ruin0verall@users.noreply.github.com>
2025-03-10 01:44:29 +0500

    fixed error for supabase

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types/database";
 
 const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_KEY!;
+const supabaseKey = process.env.SUPABASE_ANON_KEY!;
 
 export const db = createClient<Database>(supabaseUrl, supabaseKey);
