99166a705ac2b4b453ccca0202b035d22302a186
Ruin0verall <96787822+Ruin0verall@users.noreply.github.com>
2025-03-14 00:46:42 +0500

    reverts

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,5 @@
-import { createRoot, hydrateRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const rootElement = document.getElementById("root")!;
-const app = <App />;
-
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(rootElement, app);
-} else {
-  createRoot(rootElement).render(app);
-}
+createRoot(document.getElementById("root")!).render(<App />);
