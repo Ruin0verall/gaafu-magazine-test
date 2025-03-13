b49aec11bfeeb3bf3e1179c03e1c2b275bbf1f76
Ruin0verall <96787822+Ruin0verall@users.noreply.github.com>
2025-03-13 22:27:30 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { Sonner } from "@/components/ui/sonner";
+import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "./pages/Home";
+import Index from "./pages/Index";
 import Article from "./pages/Article";
 import ArticleEditor from "./pages/ArticleEditor";
 import { HelmetProvider } from "react-helmet-async";
@@ -16,11 +16,11 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Sonner />
+        <SonnerToaster />
         <HelmetProvider>
           <Router>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Index />} />
               <Route path="/article/:id" element={<Article />} />
               <Route path="/editor" element={<ArticleEditor />} />
             </Routes>
