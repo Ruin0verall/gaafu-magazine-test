9224babe983f73b2d3690f03087fe5cae1cd74b6
Ruin0verall <96787822+Ruin0verall@users.noreply.github.com>
2025-03-16 15:26:26 +0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -45,7 +45,7 @@ const Header = () => {
               <img
                 src="/Logo/Burgendary Main Colour.svg"
                 alt="Havaasa Logo"
-                className="h-8 md:h-10"
+                className="h-10 md:h-14"
               />
             </Link>
 
